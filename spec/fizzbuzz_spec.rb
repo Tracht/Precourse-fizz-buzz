9dc1ed6aeaf6e41c4213fbd34f1041f5abaef065
Marija Trachtenberg <28805811+Tracht@users.noreply.github.com>
2020-05-11 19:29:42 +0100

    fixed testing approach

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,38 +1,44 @@
- #Note also that the following two alternatives are also valid:
-#  require './lib/fizzbuzz'
-#  require_relative '../lib/fizzbuzz'
-
-# Ruby will infer the .rb extension if it is omitted,
-# so this is optional too.
-
 require 'fizzbuzz'
 
-describe 'fizz' do
-  it 'returns "fizz" when passed 3' do
-    array_3 = [3, 6, 9, 12, 18]
-    array_3.each do |num|
-      expect(fizzbuzz(num)).to eq 'fizz'
+describe 'fizzbuzz' do
+  it 'returns "fizz" for the number 3 and any multiples of 3' do
+    expect(3.fizzbuzz).to eq 'fizz'
+    expect(6.fizzbuzz).to eq 'fizz'
+    expect(18.fizzbuzz).to eq 'fizz'
+  end
+
+  it 'returns "buzz" for the number 5 and any multiples of 5' do
+    array = [5, 10, 20, 25]
+    array.each do |num|
+      expect(num.fizzbuzz).to eq 'buzz'
     end
   end
 
-  it 'returns "buzz" when passed 5' do
-    array_5 = [5, 10, 20]
-    array_5.each do |num|
-      expect(fizzbuzz(num)).to eq 'buzz'
+  it 'returns "fizzbuzz" for any mulitples of 3 & 5' do
+    array1 = [15, 30, 45]
+    array1.each do |num|
+      expect(num.fizzbuzz).to eq 'fizzbuzz'
     end
   end
 
-  it 'returns "fizzbuzz" when passed 5' do
-    array_3_5 = [15]
-    array_3_5.each do |num|
-      expect(fizzbuzz(num)).to eq 'fizzbuzz'
+  it 'returns "not in range" for numbers > 100' do
+    array2 = [101, 472, 100.5]
+    array2.each do |num|
+      expect(num.fizzbuzz).to eq 'Out of limit'
     end
   end
 
-  it 'returns "number" when passed number' do
-    array_neither = [1,2,4,7,8,11,13,14,16,17,19]
-    array_neither.each do |num|
-      expect(fizzbuzz(num)).to eq num
+  it 'returns number if number is not a mulitple of 3 or 5' do
+    array3 = [11, 27, 57]
+    array3.each do |num|
+      expect(num.fizzbuzz).to eq num
+    end
+  end
+
+  it 'returns "not in range" for numbers < 1' do
+    array3 = [0, -1020, -60777]
+    array3.each do |num|
+      expect(num.fizzbuzz).to eq 'Out of limit'
     end
   end
 
